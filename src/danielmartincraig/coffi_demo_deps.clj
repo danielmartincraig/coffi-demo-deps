01cd4735585c02909f8d89dbf665e2181e7d6918
Daniel Craig <danielmartincraig@gmail.com>
2022-12-01 21:04:52 +0000

    Added demo code

--- src/danielmartincraig/coffi_demo_deps.clj
+++ src/danielmartincraig/coffi_demo_deps.clj
@@ -1,12 +1,21 @@
 (ns danielmartincraig.coffi-demo-deps
-  (:gen-class))
+  (:gen-class)
+  (:require [coffi.mem :as mem :refer [defalias]]
+            [coffi.ffi :as ffi :refer [defcfn]]))
 
-(defn greet
+(defcfn strlen
+  "Given a string, measures its length in bytes."
+  strlen [::mem/c-string] ::mem/long)
+
+(defn process-input
   "Callable entry point to the application."
   [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+  (println (str "The length of the input string is: " (strlen (or (:user-input data) "")))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Count the length of the input string"
   [& args]
-  (greet {:name (first args)}))
+  (process-input {:user-input (first args)}))
+
+(-main "Daniel")
+
